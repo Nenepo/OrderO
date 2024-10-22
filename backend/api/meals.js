da8e89c8aaedd5dec25b9249c7ecfe49c42e5712
Nafisa <Nafisafaruna@gmail.com>
2024-10-22 19:06:57 +0100

    j

--- backend/api/meals.js
+++ backend/api/meals.js
@@ -3,7 +3,7 @@ import { promises as fs } from 'node:fs';
 export default async function handler(req, res) {
   if (req.method === 'GET') {
     try {
-      const meals = await fs.readFile('./data/available-meals.json', 'utf8');
+      const meals = await fs.readFile('../data/available-meals.json', 'utf8');
       res.status(200).json(JSON.parse(meals));
     } catch (error) {
       res.status(500).json({ message: 'Failed to load meals' });
--- backend/api/orders.js
+++ backend/api/orders.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
         ...orderData,
         id: (Math.random() * 1000).toString(),
       };
-      const orders = await fs.readFile('./data/orders.json', 'utf8');
+      const orders = await fs.readFile('../data/orders.json', 'utf8');
       const allOrders = JSON.parse(orders);
       allOrders.push(newOrder);
       await fs.writeFile('./data/orders.json', JSON.stringify(allOrders));

3577132627e95b3abe289a85634c2c0668c23b53
Nafisa <Nafisafaruna@gmail.com>
2024-10-23 10:05:50 +0100

    naawah

--- backend/api/meals.js
+++ backend/api/meals.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
 
   if (req.method === 'GET') {
     try {
-      const meals = await fs.readFile('../data/available-meals.json', 'utf8');
+      const meals = await fs.readFile('./data/available-meals.json', 'utf8');
       res.status(200).json(JSON.parse(meals));
     } catch (error) {
       res.status(500).json({ message: 'Failed to fetch meals.' });
